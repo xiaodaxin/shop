cc5f88f9a6d8befd7fa1e0bdcc41994e0ef4d414
xiaodaxin <gisxx@qq.com>
2018-03-10 22:08:02 +0800

    create

---
+++ WebContent/admin/js/add.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+    /**
+     * 上传图片部分
+     * */
+    $("#upload-img").click(function () {
+        document.getElementById("imgFile").click();
+    });
+    var x = new FileReader;
+    document.forms[0].elements[0].onchange = function () {
+        x.readAsDataURL(this.files[0]);
+    };
+    x.onloadend = function () {
+        //document.images[0].src=this.result;
+        $("#upload-img").css("background-image", "url(" + this.result + ")");
+    };
+
+    /**
+     * 获得商品类别
+     * */
+    $.ajax({
+        url:"http://localhost:8080/SHOP/admin/queryAllCategory",
+        success:function(data){
+            var json=JSON.parse(data);
+            for(var i=0;i<json.length;i++){
+                $("#categoryId").append("<option value='"+json[i].id+"'>"+json[i].name+"</option>")
+            }
+        }});
+
+});
